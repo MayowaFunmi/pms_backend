0ddc8a3718bf54dd5f30394ae18c70653634d79f
Akinade Mayowa Emmnauel <akinade.mayowa@gmail.com>
2021-01-02 15:39:06 +0100

    Update models.py

--- hotel/models.py
+++ hotel/models.py
@@ -10,12 +10,29 @@ class Floor(models.Model):
     floor_number = models.SmallIntegerField()
 
 
-class Room(models.Model):
-    building_no = models.OneToOneField(Building, on_delete=models.CASCADE)
-    floor_no = models.OneToOneField(Floor, on_delete=models.CASCADE)
+class RoomCategory(models.Model):
     ROOM_TYPE = [
         ('Standard', 'Standard'),
         ('Single', 'Single')
     ]
 
     room_type = models.CharField(choices=ROOM_TYPE, max_length=50)
+    rate = models.IntegerField()
+    beds = models.PositiveIntegerField()
+    capacity = models.PositiveIntegerField()
+    size = models.CharField(max_length=59)
+
+    def __str__(self):
+        return f"{self.room_type} room costs N{self.rate}"
+
+
+class Room(models.Model):
+    room_category = models.ForeignKey(RoomCategory, on_delete=models.CASCADE)
+    building_no = models.OneToOneField(Building, on_delete=models.CASCADE)
+    floor_no = models.OneToOneField(Floor, on_delete=models.CASCADE)
+    room_number = models.SmallIntegerField()
+    availability = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"The room {self.room_number} {self.room_category} has a maximum of {self.room_category.capacity} person " \
+               f"and cost {self.room_category.rate}/night "
